fe80640ae689e8820314e761e9250336b997adce
woojin_jung <othlvhto@gmail.com>
2022-10-06 11:51:09 +0900

    Create register_renaming_unit.sv

---
+++ src/register_renaming_unit.sv
@@ -0,0 +1,32 @@
+//=======================================================
+// register_renaming_unit.sv
+//
+// author: Woojin Jung
+//=======================================================
+
+import Type::*;
+
+module RegRenamingUnit #(
+  
+)(
+  input CLK,
+  input RSTn,
+  
+  input Packet::DecoderToRru packetFromRru,
+  
+  output Packet::RruToRob1 packetToRob1,
+  output Packet::RruToRob2 pacektToRob2
+);
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+endmodule
