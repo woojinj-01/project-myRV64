fd50d0e9584a0524c78ecb18d4e30ee5d79e3509
woojin_jung <othlvhto@gmail.com>
2022-10-06 16:17:24 +0900

    update on some functions

--- src/register_renaming_unit.sv
+++ src/register_renaming_unit.sv
@@ -22,21 +22,25 @@ module RegRenamingUnit #(
             CURRENTMAP_START = 13, CURRENTMAP_END = 7,
             PASTMAP_START = 6, PASTMAP_END = 0;
   
-  parameter SIZE_PHYREGPTR = 7 //log2(NUM_PHYREG)
+  typedef enum{MAP_CONSUMER, MAP_PRODUCER}      mapOption_t;
+  typedef logic[VALID_START:PASTMAP_END] mapTable_t[0:NUM_ARCHREG-1];
+  typedef logic[0:NUM_PHYREG-1] bitmap_t;
+  typedef logic[SIZE_PHYREGPTR-1:0] phyRegPtr_t;
   
-  logic[0:NUM_ARCHREG-1] mapTable [VALID_START:PASTMAP_END];
-  logic[0:NUM_PHYREG-1] phyRegBitmap;    //manage resource availability
-  logic[SIZE_PHYREGPTR-1:0] phyRegPtr;  //pointer to phyreg bitmap
+  parameter SIZE_PHYREGPTR = 7 //log2(NUM_PHYREG)
   
   RobIndex_T dstRobIndex;
   ArchRegisterId_T dstArchReg, srcArchReg1, srcArchReg2;
   PhyRegisterId_T dstPhyReg, srcPhyReg1, srcPhyReg2, prevMappedPhyReg;
+  maptable_t mapTable;
+  bitmap_t phyRegBitmap;
+  phyRegPtr_t phyRegPtr;
   
   
   function void resetToZero(
-    logic[0:NUM_ARCHREG-1] mapTable [VALID_START:PASTMAP_END],
-    logic[0:NUM_PHYREG-1] phyRegBitmap,
-    logic[SIZE_PHYREGPTR-1:0] phyRegPtr
+    input mapTable_t mapTable,
+    input bitmap_t phyRegBitmap,
+    input phyRegPtr_t phyRegPtr
   );
     int i;
     
@@ -49,15 +53,57 @@ module RegRenamingUnit #(
     
   endfunction
   
+  function PhyRegisterId_T phyRegToMap(
+    input ArchRegisterId_T archReg,
+    input mapTable_t mapTable,
+    input bitmap_t phyRegBitmap,
+    input phyRegPtr_t phyRegPtr
+    input mapOption_t option,
+  );
+    if(MAP_CONSUMER == option) begin
+      return mapTable[archReg][CURRENTMAP_START:CURRENTMAP_END];
+    end
+    else if(MAP_PRODUCER == option) begin
+      return findNewPhyReg(
+       phyRegBitmap, phyRegPtr
+      );
+    end
+  endfunction
+  
+  function findNewPhyReg(
+    input bitmap_t phyRegBitmap,
+    input phyRegPtr_t phyRegPtr
+  );
+    /*
+    
+      Algorithm to find new phyreg.
+    
+    */
+  endfunction
+    
+  
   
   always_comb begin
     Packet::DecapDecoderToRru(
         packetFromRlu, 
         dstRobIndex, dstArchReg, srcArchReg1, srcArchReg2
       );
+    //map src1 (should atomically update bitmap&maptable)
+    srcPhyReg1 = phyRegToMap(
+      srcArchReg1, mapTable, phyRegBitmap, phyRegPtr, MAP_CONSUMER
+    );
+    //map src2 (should atomically update bitmap&maptable)
+    srcPhyReg2 = phyRegToMap(
+      srcArchReg2, mapTable, phyRegBitmap, phyRegPtr, MAP_CONSUMER
+    );
+    //map dst (should atomically update bitmap&maptable)
+    dstPhyReg = phyRegToMap(
+      dstArchReg, mapTable, phyRegBitmap, phyRegPtr, MAP_PRODUCER
+    );
     /*
       
           Intermediate Stages have to be filled.
+          Also, prevMappedReg should be assigned. (not at the above code yet)
       
       */
     
